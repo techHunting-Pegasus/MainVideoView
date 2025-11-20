5b66b3e140a1d971158d67aa5b82eeeb9cb7eeb0
Ishpreet  singh <Ishpreet.s@multitvsolution.com>
2025-11-20 18:02:57 +0530

    Added custom video player overlay config

--- Sources/MainVideoView/MainVideoView.swift
+++ Sources/MainVideoView/MainVideoView.swift
@@ -31,31 +31,159 @@ struct VideoPlayerContainer: UIViewControllerRepresentable {
 
 
 public struct VideoViewPlayer: View {
-    @ObservedObject var viewModel: VideoPlayerViewModel
+    @ObservedObject var vm: VideoPlayerViewModel
         let config: VideoPlayerConfig
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    @State private var brightness: CGFloat = UIScreen.main.brightness
+    @State private var localBrightness: CGFloat = UIScreen.main.brightness
+    @State private var localVolume: Float = AVAudioSession.sharedInstance().outputVolume
+    @State private var activeSide: String? = nil   // "left" or "right"
+    @State private var showOverlay = false
+    @State var showSettings : Bool = false
+    let delegate: VideoViewDelegate?
         
-        public init(viewModel: VideoPlayerViewModel, config: VideoPlayerConfig = .init()) {
-            self.viewModel = viewModel
+        public init(viewModel: VideoPlayerViewModel, config: VideoPlayerConfig = .init(), delegate: VideoViewDelegate? = nil) {
+            self.delegate = delegate
+            self.vm = viewModel
             self.config = config
+            let thumbImage = UIImage(systemName: "circle.fill")?
+                .withConfiguration(UIImage.SymbolConfiguration(pointSize: 20))
+                .withTintColor(.white, renderingMode: .alwaysOriginal)
+            UISlider.appearance().setThumbImage(thumbImage, for: .normal)
         }
+    
         
     public var body: some View {
-           ZStack {
-               VideoPlayerContainer(player: viewModel.player, config: config)
+        GeometryReader { geo in
+            ZStack {
+                VideoPlayerContainer(player: vm.player, config: config)
+                    .onTapGesture {
+                        withAnimation { /*vm.showControls.toggle() */}
+                    }
+                    .frame(
+                        width: vm.isfilled ? screenHeight : geo.size.width,
+                        height: vm.isfullScreen ? screenWidth : geo.size.width * 9 / 16
+                    )
+                Color.clear
+                    .contentShape(Rectangle())
+                    .gesture(
+                        DragGesture(minimumDistance: 10)
+                            .onChanged { value in
+                                let dragAmount = value.translation.height
+                                let screenHalf = geo.size.width / 2
+                                let isLeftSide = value.startLocation.x < screenHalf
+                                
+                                if isLeftSide {
+                                    activeSide = "left"
+                                    // Brightness control
+                                    localBrightness = max(0.0, min(1.0, brightness - dragAmount / 300))
+                                    
+                                    // Update screen brightness smoothly
+                                    DispatchQueue.main.async {
+                                        UIScreen.main.brightness = localBrightness
+                                    }
+                                } else {
+                                    activeSide = "right"
+                                    // Volume control
+                                    let newVolume = max(0.0, min(1.0, localVolume - Float(dragAmount / 300)))
+                                    vm.volume = newVolume  // update your player volume
+                                }
+                                
+                                withAnimation { showOverlay = true }
+                            }
+                            .onEnded { _ in
+                                brightness = UIScreen.main.brightness
+                                localVolume = vm.volume
+                                withAnimation(.easeOut(duration: 0.5)) {
+                                    showOverlay = false
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                                    activeSide = nil
+                                }
+                            }
+                    )
+                if showOverlay, activeSide == "left" {
+                    VStack(alignment:.leading) {
+                        Slider(
+                            value: Binding(
+                                get: { Double(localBrightness) },
+                                set: { newValue in
+                                    localBrightness = CGFloat(newValue)
+                                    UIScreen.main.brightness = localBrightness
+                                }
+                            ),
+                            in: 0...1
+                        )
+                        .rotationEffect(.degrees(-90))
+                        .frame(width: 100)
+                        .accentColor(.yellow)
+                        .tint(.yellow)
+                        .padding(.leading, 10)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .transition(.opacity)
+                    
+                }
+                
+                // MARK: - Show Right (Volume) Slider
+                if showOverlay, activeSide == "right" {
+                    VStack {
+                        Slider(
+                            value: Binding(
+                                get: { Double(vm.volume) },
+                                set: { vm.volume = Float($0) }
+                            ),
+                            in: 0...1
+                        )
+                        .rotationEffect(.degrees(-90))
+                        .frame(width: 100)
+                        .accentColor(.red)
+                        .tint(.white)
+                        .padding(.trailing, 10)
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .transition(.opacity)
+                }
+                if vm.isBuffering {
+                        ZStack {
+                            Color.black.opacity(0.4)
+                                .ignoresSafeArea()
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                                .scaleEffect(2)
+                        }
+                        .transition(.opacity)
+                        .animation(.easeInOut(duration: 0.25), value: vm.isBuffering)
+                    }
+                
+                
+                if  vm.showControls && !showSettings {
+                    
+                    if (!vm.isfullScreen){
+                        PotraghtControle(vm: vm, delegate: delegate)
+                    }else{
+//                        LandscapdeControls(vm: vm, delegate: delegate, screenwidth: screenWidth, screenheight: screenHeight, setting: $showSettings)
+//                            .frame(width: geo.size.width)
+                    }
+                }
 
-               
-                   Button {
-                       viewModel.playPause()
-                   } label: {
-                       if viewModel.isPlaying {
-                           config.pauseButtonView ?? AnyView(defaultPauseButton)
-                       } else {
-                           config.playButtonView ?? AnyView(defaultPlayButton)
-                       }
-                   }
-               
-           }
-       }
+                
+//                    Button {
+//                        vm.playPause()
+//                    } label: {
+//                        if vm.isPlaying {
+//                            config.pauseButtonView ?? AnyView(defaultPauseButton)
+//                        } else {
+//                            config.playButtonView ?? AnyView(defaultPlayButton)
+//                        }
+//                    }
+                
+            }
+        }
+        }
+        
     private var defaultPlayButton: some View {
            Image(systemName: "play.circle.fill")
                .resizable()
@@ -71,5 +199,88 @@ public struct VideoViewPlayer: View {
        }
 }
 
+public protocol VideoViewDelegate {
+    func videoDidPlay()
+    func videoDidPause()
+    func videoDidFinish()
+    func isfullscren(isfull: Bool)
+}
 
 
+extension VideoViewDelegate {
+    func videoDidPlay() {}
+    func videoDidPause() {}
+    func videoDidFinish() {}
+    func isfullscren(isfull: Bool) {}
+}
+
+struct PotraghtControle : View {
+    
+    
+    @ObservedObject var vm: VideoPlayerViewModel
+    var delegate: VideoViewDelegate?
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            // Control Buttons
+            HStack(spacing: 40) {
+                Button { vm.rewind10s() } label: { Image(systemName: "gobackward.10").font(.title).tint(.white) }
+                if vm.isBuffering {
+                    ZStack {
+                        Color.black.opacity(0.3)
+                            .ignoresSafeArea()
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                            .scaleEffect(1.8)
+                    }
+                    .transition(.opacity)
+                    .animation(.easeInOut(duration: 0.3), value: vm.isBuffering)
+                }else{
+                    Button { vm.togglePlayPause() } label: { Image(systemName: vm.isPlaying ? "pause.fill" : "play.fill").font(.title).tint(.white) }
+                }
+              
+                
+                Button { vm.forward10s() } label: { Image(systemName: "goforward.10").font(.title).tint(.white) }
+            }
+            .padding(.top,30)
+            
+            
+            Spacer()
+            
+            HStack {
+                
+                
+                Spacer()
+                
+                HStack(spacing:20) {
+                    Image(systemName: vm.ismute ? "speaker.slash.fill" : "speaker.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    
+                        .foregroundColor(.white)
+                        .onTapGesture { vm.toggleMute() }
+                    
+                    
+                    Image(systemName: "viewfinder")
+                        .resizable()
+                        .frame(width: 30,height: 30)
+                        .foregroundStyle(.white)
+                        .onTapGesture {
+                            
+                            vm.toggleFullscreen()
+                            if vm.isfullScreen{
+                                delegate?.isfullscren(isfull: true)
+                                
+                            }else{
+                                delegate?.isfullscren(isfull: false)
+                            }
+                        }
+                    
+                }
+            }
+            .padding()
+            
+        }
+    }
+}
--- Sources/MainVideoView/VideoPlayerViewModel.swift
+++ Sources/MainVideoView/VideoPlayerViewModel.swift
@@ -2,23 +2,65 @@
 
 import AVFoundation
 import Combine
+import UIKit
 
 public class VideoPlayerViewModel: ObservableObject {
     let player: AVPlayer
     @Published var isPlaying: Bool = false
+    @Published var isfilled:Bool = false
+    @Published var isfullScreen:Bool = false
+    @Published var ismute:Bool = false
+    @Published var volume: Float = 1.0 { // default full volume
+            didSet {
+                player.volume = volume
+                ismute = volume == 0
+            }
+        }
+    @Published var isBuffering: Bool = false
+    @Published var showControls: Bool = true
+    @Published var progress: Double = 0
+    private var bitrateResetWorkItem: DispatchWorkItem?
+    var delegate : VideoViewDelegate?
     
-    public  init(url: URL) {
-        self.player = AVPlayer(url: url)
+    
+    public  init(url: URL,autoplay: Bool = true,delegate:VideoViewDelegate? = nil) {
+        
+        let asset = AVURLAsset(url: url)
+        let item = AVPlayerItem(asset: asset)
+        
+        
+        
+        item.preferredPeakBitRate = 50_000
+        
+        
+        self.player = AVPlayer(playerItem: item)
+        
+        player.automaticallyWaitsToMinimizeStalling = false
+        self.delegate = delegate
+       
+        
+        
+        if autoplay {
+            player.play()
+            isPlaying = true
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            item.preferredPeakBitRate = 0
+        })
+        
     }
     
-    public func playPause() {
-            if isPlaying {
-                player.pause()
-            } else {
-                player.play()
-            }
-            isPlaying.toggle()
+    public  func togglePlayPause() {
+        if isPlaying {
+            player.pause()
+            delegate?.videoDidPause()
+        } else {
+            player.play()
+            delegate?.videoDidPlay()
         }
+        isPlaying.toggle()
+    }
     
    
     
@@ -27,4 +69,90 @@ public class VideoPlayerViewModel: ObservableObject {
            let target = max(0, current + seconds)
            player.seek(to: CMTime(seconds: target, preferredTimescale: 600))
        }
+    
+    func rewind10s() {
+        let current = player.currentTime().seconds
+        let newTime = max(current - 10, 0)
+        let shouldResume = player.timeControlStatus == .playing
+        updateProgressImmediately(to: newTime)
+        smoothSeek(to: newTime) {
+            if shouldResume { self.player.play() }
+        }
+    }
+    func forward10s() {
+        guard let duration = player.currentItem?.duration.seconds else { return }
+        let current = player.currentTime().seconds
+        let newTime = min(current + 10, duration)
+        let shouldResume = player.timeControlStatus == .playing
+        updateProgressImmediately(to: newTime)
+        smoothSeek(to: newTime) {
+            if shouldResume { self.player.play() }
+        }
+    }
+    private func updateProgressImmediately(to time: Double) {
+        // Immediately reflect the new slider position
+        
+        DispatchQueue.main.async {
+            self.progress = time
+        }
+    }
+    func toggleMute() {
+        if ismute {
+            // Restore to previous volume
+            volume = 1.0
+        } else {
+            // Mute (set to 0)
+            volume = 0.0
+        }
+    }
+    
+    private func smoothSeek(to seconds: Double, completion: (() -> Void)? = nil) {
+        let wasPlaying = player.timeControlStatus == .playing
+        let target = CMTime(seconds: seconds, preferredTimescale: 600)
+        let tolerance = CMTime(seconds: 0.05, preferredTimescale: 600)
+
+        // Temporarily lower bitrate
+        player.currentItem?.preferredPeakBitRate = 50_000
+
+        player.seek(to: target, toleranceBefore: tolerance, toleranceAfter: tolerance) { [weak self] _ in
+            guard let self = self else { return }
+
+
+            self.scheduleBitrateReset()
+            completion?()
+        }
+    }
+    private func scheduleBitrateReset() {
+        
+        bitrateResetWorkItem?.cancel()
+        let workItem = DispatchWorkItem { [weak self] in
+            self?.player.currentItem?.preferredPeakBitRate = 0
+        }
+        bitrateResetWorkItem = workItem
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6, execute: workItem)
+    }
+    
+   @MainActor func  toggleFullscreen(){
+        if isfullScreen{
+            OrientationHelper.setOrientation(.portrait)
+            isfullScreen = false
+        }else{
+            OrientationHelper.setOrientation(.landscapeRight)
+            isfullScreen = true
+        }
+    }
+}
+@MainActor
+
+struct OrientationHelper {
+    static func setOrientation(_ orientation: UIInterfaceOrientationMask) {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+        
+        // Tell the system to rotate
+        windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: orientation)) { error in
+            if error != nil {
+                print("Orientation update error: \(error.localizedDescription)")
+            }
+        }
+    }
 }
